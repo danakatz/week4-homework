bb7f8a3c37812153e3336e010a64b05ebe9a00e1
Dana Katz <danakatz@me.com>
2015-04-28 16:17:29 -0500

    RESTful redesign

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -4,14 +4,10 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
-  def show
-    @place = Place.find(params[:id])
-  end
-
   def new
   end
 
-  def submitted
+  def create
     Place.create title: params[:title],
                  price: (params[:price].to_f * 100).to_i,
                  photo_url: params[:url],
@@ -20,24 +16,36 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+    @reviews = Review.where(place_id: @place.id).order('id desc')
+  end
+
   def edit
     @place = Place.find(params[:id])
   end
 
   def update
     @place = Place.find(params[:id])
-    @place.update title: params[:title],
-                 price: (params[:price].to_f * 100).to_i,
-                 photo_url: params[:url],
-                 desc: params[:desc]
 
-    redirect_to "/places/#{@place.id}"
+    if params[:title].nil? 
+      Review.create place_id: @place.id,
+                    rating: params[:rating].to_i,
+                    desc: params[:desc]
+    else
+      @place.update title: params[:title],
+                    price: (params[:price].to_f * 100).to_i,
+                    photo_url: params[:url],
+                    desc: params[:desc]
+    end
+    
+    redirect_to place_url(@place.id)
   end
 
-  def delete
+  def destroy
     Place.delete(params[:id])
+    Review.where(place_id: params[:id]).delete_all
     redirect_to root_path
   end
-
 
 end
